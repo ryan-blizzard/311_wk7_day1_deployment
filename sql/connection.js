d065288c0b32319ab0ad4aed125a58135db9d555
ryan-blizzard <blizzy34@hotmail.com>
2020-02-25 19:06:36 -0600

    update

--- sql/connection.js
+++ sql/connection.js
@@ -7,9 +7,9 @@ class Connection {
 
       const config = {
         connectionLimit: 100,
-        host: 'localhost',
+        host: '35.238.250.54',
         user: 'root',
-        password: 'password',
+        password: 'tatertot34',
         database: 'admin'
       }
 
